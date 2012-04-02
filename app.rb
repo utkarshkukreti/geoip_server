ed36527de07d75afb87df0ee361709060d61dd93
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-04-02 11:58:42 +0530

    Serve blank content on /

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ configure do
   DB = GeoIP::City.new(File.expand_path(File.join(File.dirname(__FILE__), "GeoLiteCity.dat")))
 end
 
+get '/' do
+end
+
 get '/json/:ip' do |ip|
   content_type "application/json", charset: "utf-8"
   DB.look_up(ip).to_json
